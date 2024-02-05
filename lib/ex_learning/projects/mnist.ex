eb336fac67ef037587030b7cbc4c0d83a12f48e7
Jia Wang <wj2016@gmail.com>
2024-02-05 17:23:25 +0100

    清理之前Mac错误的优化代码

--- lib/ex_learning/projects/mnist.ex
+++ lib/ex_learning/projects/mnist.ex
@@ -34,10 +34,6 @@ defmodule ExLearning.Projects.Mnist do
     load_images_file(@train_image_file)
   end
 
-  def load_train_labels() do
-    load_labels_file(@train_label_file)
-  end
-
   def load_one_hot_encoded_train_labels() do
     <<_::32, _n_labels::32, labels::binary>> = read_and_unzip!(@train_label_file)
 
@@ -62,23 +58,11 @@ defmodule ExLearning.Projects.Mnist do
   end
 
   def load_test_labels() do
-    load_labels_file(@test_label_file)
-  end
-
-  defp load_labels_file(filename) do
-    <<_::32, _n_labels::32, labels::binary>> = read_and_unzip!(filename)
+    <<_::32, _n_labels::32, labels::binary>> = read_and_unzip!(@test_label_file)
 
     labels
-    |> :erlang.binary_to_list()
-    |> Stream.map(&normalize_num_5/1)
-    |> Stream.map(fn n -> [n] end)
-    |> Enum.to_list()
-    |> Nx.tensor()
+    |> Nx.from_binary({:u, 8})
   end
-
-  # MNIST先简单做二分判断是否为5的特例
-  defp normalize_num_5(5), do: 1
-  defp normalize_num_5(_), do: 0
 
   defp load_images_file(filename) do
     <<_::32, n_images::32, n_rows::32, n_cols::32, images_bin::binary>> =
@@ -89,32 +73,6 @@ defmodule ExLearning.Projects.Mnist do
     |> Nx.from_binary({:u, 8})
     |> Nx.reshape({n_images, n_rows * n_cols})
     |> Util.append_column(1)
-  end
-
-  # 这是我之前针对Mac CPU后端所做的"优化"但是cuda之后concatenate很慢
-  def image_bin_to_input_tensor(images_bin, image_size) do
-    do_images_to_tensor(images_bin, image_size, 1, [])
-  end
-
-  defp do_images_to_tensor(<<>>, _, _, tensors) do
-    tensors
-    |> Enum.reverse()
-    |> Nx.concatenate()
-  end
-
-  defp do_images_to_tensor(images, size, num, acc) do
-    <<image::binary-size(^size), rest::binary>> = images
-
-    t =
-      prepend_bias_column(image, num)
-      |> Nx.from_binary({:u, 8})
-      |> Nx.reshape({1, size + 1})
-
-    do_images_to_tensor(rest, size, num, [t | acc])
-  end
-
-  defp prepend_bias_column(image_bin, num) do
-    <<num, image_bin::binary>>
   end
 
   defp read_and_unzip!(filename) do
