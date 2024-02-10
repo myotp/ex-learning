4ee74a789cd4f571a8a1ec95bd72eff576e5be7d
Jia Wang <wj2016@gmail.com>
2024-02-10 21:42:00 +0100

    add alias

--- .iex.exs
+++ .iex.exs
@@ -2,9 +2,10 @@ alias ExLearning.Projects.Pizza
 alias ExLearning.Projects.MultiPizza
 alias ExLearning.Projects.Police
 alias ExLearning.Projects.Digit5
-alias ExLearning.Projects.Mnist
+alias ExLearning.Projects.MnistRegression
 alias ExLearning.Projects.MnistNN
 alias ExLearning.Projects.MnistAxon
+alias ExLearning.Projects.MnistCnn
 
 defmodule ExLearning.IExHelpers do
   defdelegate r(), to: IEx.Helpers, as: :recompile
